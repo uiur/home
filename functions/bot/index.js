71160f00dd8a6275d526ff2fe1b0b0dba9ede5bd
Kazato Sugimoto <uiureo@gmail.com>
2016-07-06 03:48:22 +0900

    Add bot command

--- functions/bot/index.js
+++ functions/bot/index.js
@@ -1,8 +1,13 @@
+'use strict'
+
 var AWS = require('aws-sdk')
 var qs = require('querystring')
+var exec = require('child_process').exec
+var axios = require('axios')
 var token, kmsEncyptedToken
 
 kmsEncyptedToken = 'CiAt+TsuI6Hi2oKVHYHhBfa+QRilif61RHGjpi7zsgQPmBKfAQEBAgB4Lfk7LiOh4tqClR2B4QX2vkEYpYn+tURxo6Yu87IED5gAAAB2MHQGCSqGSIb3DQEHBqBnMGUCAQAwYAYJKoZIhvcNAQcBMB4GCWCGSAFlAwQBLjARBAxJX8JQ7gipo3J/PZgCARCAM8h8QdsPyCiPhB76MJerdkvJ6gst4uEG5CAjU7/vQDpg9PzGlnjmSKrIc7D8bN46Nc11pg=='
+
 
 exports.handle = function (event, context) {
   if (token) {
@@ -28,6 +33,10 @@ exports.handle = function (event, context) {
   }
 }
 
+function publish (topic, payload) {
+  return axios.post(`https://84ry1pi5zb.execute-api.ap-northeast-1.amazonaws.com/prod/publish?topic=${topic}`, payload)
+}
+
 var processEvent = function (event, context) {
   var body = event.body
   var params = qs.parse(body)
@@ -40,8 +49,31 @@ var processEvent = function (event, context) {
   var user = params.user_name
   var channel = params.channel_name
   var commandText = params.text
+  var text = commandText.replace(/^bot\s+/, '')
 
-  context.succeed({
-    text: 'こんにちは〜'
-  })
+  var topic
+  var payload
+  if ((/でんきつけて/).test(text)) {
+    topic = 'lights'
+    payload = { on: true }
+  } else if ((/でんきけして/).test(text)) {
+    topic = 'lights'
+    payload = { on: false }
+  } else if ((/エアコンつけて/).test(text)) {
+    topic = 'aircon'
+    payload = { on: true }
+  } else if ((/エアコンけして/)) {
+    topic = 'aircon'
+    payload = { on: false }
+  }
+
+  publish(topic, payload)
+    .then(() => {
+      context.succeed({
+        text: 'ok'
+      })
+    })
+    .catch(err => {
+      console.error(err)
+    })
 }
