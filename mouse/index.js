a794f42a0d40e1455ddad361142cab21990167d3
Kazato Sugimoto <uiureo@gmail.com>
2016-08-18 22:25:43 +0900

    Change brightness using wheel

--- mouse/index.js
+++ mouse/index.js
@@ -1,19 +1,48 @@
 const request = require('axios')
 const Mouse = require('node-mouse')
-const m = new Mouse()
+const InputEvent = require('input-event')
+const throttle = require('lodash.throttle')
+
+const mouseDevice = new Mouse()
+
+const input = new InputEvent('/dev/input/event0')
+const eventDevice = new InputEvent.Mouse(input)
 
 const groupUrl = 'http://10.0.1.2/api/UDFYuTwbCfOgWUifr60MaAEBTDMmGAjxEzWkaRyA/groups/0'
 
-let lightState = { on: false }
-m.on('mouseup', e => {
+mouseDevice.on('click', e => {
   console.log(e)
-
-  lightState.on = !lightState.on
 
   const isLeftClick = e.button === 0
   if (isLeftClick) {
-    request.put(`${groupUrl}/action`, lightState).then(res => {
-      console.log(res.data)
-    }).catch(err => console.error(err))
+    request.get(groupUrl).then(res => {
+      let lightState = { on: res.data.on }
+      lightState.on = !lightState.on
+
+      return request.put(`${groupUrl}/action`, lightState)
+    })
+      .then(res => console.log(res.data))
+      .catch(console.error)
   }
 })
+
+let delta = 0
+eventDevice.on('wheel', e => {
+  console.log(e)
+
+  const direction = e.value === 1 ? 1 : -1
+  delta += direction * 10
+})
+
+eventDevice.on('wheel', throttle(e => {
+  request.get(groupUrl).then(res => {
+    let bri = res.data.action.bri + delta
+    bri = Math.max(0, Math.min(255, bri))
+    delta = 0
+
+    request.put(`${groupUrl}/action`, { bri: bri })
+      .then(res => res.data)
+      .then(console.log)
+      .catch(console.error)
+  })
+}, 200))
